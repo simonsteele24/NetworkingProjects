aec03601b91dfb42abb23260cc7a8df34440e920
Chun <chuntao.lin@mymail.champlain.edu>
2021-02-21 14:12:43 -0500

    Added a money and betting. not entirely sure if i got the rules right but the Money and betting works in standalone non networked BJ

--- SimpleBlackJackWithoutMultiplayer/SimpleBlackJack/SimpleBlackJack/SimpleBlackJack.cpp
+++ SimpleBlackJackWithoutMultiplayer/SimpleBlackJack/SimpleBlackJack/SimpleBlackJack.cpp
@@ -11,6 +11,7 @@
 struct Player 
 {
 	std::vector<int> cards;
+	float money, bet = 0;
 };
 
 int CalculateScore(const std::vector<int>& cards)
@@ -28,6 +29,117 @@ int CalculateScore(const std::vector<int>& cards)
 int PickRandomCard()
 {
 	return rand() % 10 + 1;
+}
+
+std::string DetermineWinner(Player& dealer, Player& player)
+{
+	std::string output;
+	int dealerVal , playerVal;
+
+	dealerVal = CalculateScore(dealer.cards);
+	playerVal = CalculateScore(player.cards);
+	
+	//First check Dealer has more value than dealer
+	if (dealerVal > playerVal)
+	{
+		//dealer busted
+		if (dealerVal > 21)
+		{
+			//player bustsed
+			if (playerVal > 21)
+			{
+				output = "Everyone busted, Dealer wins by default";
+				player.money -= player.bet + dealer.bet;
+				dealer.money += player.bet + dealer.bet;
+				player.bet = 0;
+				dealer.bet = 0;
+			}
+			//player wins
+			else
+			{
+				output = "Player Won";
+				player.money += player.bet + dealer.bet;
+				dealer.money -= dealer.bet;
+				player.bet = 0;
+				dealer.bet = 0;
+			}				
+		}
+		else
+		{
+			output = "Dealer Won";
+			dealer.money += player.bet + dealer.bet;
+			player.money -= player.bet;
+			dealer.bet = 0;
+			player.bet = 0;
+		}
+	}
+	//Second check Dealer has more value than player
+	else if (playerVal > dealerVal)
+	{
+		//dealer busted
+		if (dealerVal > 21)
+		{
+			//player bustsed
+			if (playerVal > 21)
+			{
+				output = "Everyone busted, Dealer wins by default";
+				player.money -= player.bet + dealer.bet;
+				dealer.money += player.bet + dealer.bet;
+				player.bet = 0;
+				dealer.bet = 0;
+			}
+			//player wins
+			else
+			{
+				output = "Player Won";
+				player.money += player.bet + dealer.bet;
+				dealer.money -= dealer.bet;
+				player.bet = 0;
+				dealer.bet = 0;
+			}
+		}
+		else
+		{
+			output = "Dealer Won";
+			dealer.money += player.bet + dealer.bet;
+			player.money -= player.bet;
+			dealer.bet = 0;
+			player.bet = 0;
+		}
+	}
+	else
+	{
+		output = "NO winner";
+		dealer.bet = 0;
+		player.bet = 0;
+	}
+
+	return output;
+}
+void PlaceBet(float& bet, float money)
+{
+	std::string input;
+	
+	std::cout << "Input Number to Bet: ";
+	std::cin >> input;
+
+	if ((float)std::stoi(input) > money)
+	{
+		do
+		{
+			std::cout << "Not Enough Money on hand.." << "\n";
+			std::cout << "Input Number to Bet: ";
+			std::cin >> input;
+		} while ((float)std::stoi(input) > money);
+
+	}
+
+	if (input.size() == 0)
+	{
+		bet = 0;
+	}
+	else
+		bet = (float)std::stoi(input);
 }
 
 std::string DisplayCards(const std::vector<int> & cards) 
@@ -89,11 +201,21 @@ void RunoutRestOfDealerInterface(std::vector<Player>& players)
 int main()
 {
 	std::srand(time(NULL));
+	//Initialize vector of players and money
 	std::vector<Player> players;
 	players.push_back(Player());
 	players.push_back(Player());
+	players[0].money = 1000;
+	players[1].money = 1000;
 
 	bool inGame = true;
+
+	std::cout << "Place Bets Now";
+
+	std::cout << "\n";
+
+	PlaceBet(players[0].bet,players[0].money);	
+	PlaceBet(players[1].bet, players[1].money);
 
 	while (inGame)
 	{
@@ -114,6 +236,14 @@ int main()
 		std::string input = "";
 		int numInput = 0;
 
+		std::cout << DetermineWinner(players[0],players[1]);
+
+		std::cout << "\n";
+
+		std::cout << "Money: " << "\n";
+		std::cout << "Dealer: " << std::to_string(players[0].money) << "\n";
+		std::cout << "Player: " << std::to_string(players[1].money) << "\n";
+
 		std::cout << "Play again (0): ";
 		std::cin >> input;
 
@@ -124,6 +254,12 @@ int main()
 		switch (numInput)
 		{
 		case 0:
+			std::cout << "Place Bets Now";
+
+			std::cout << "\n";
+
+			PlaceBet(players[0].bet, players[0].money);
+			PlaceBet(players[1].bet, players[1].money);
 			break;
 		default:
 			inGame = false;
