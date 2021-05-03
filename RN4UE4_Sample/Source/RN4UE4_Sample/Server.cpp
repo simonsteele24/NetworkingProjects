4d35da7b3996cfbcc34046ad4ff884839c0ed3e3
Simon Steele <simon.steele@mymail.champlain.edu>
2021-05-03 18:45:11 -0400

    Commented out server

--- RN4UE4_Sample/Source/RN4UE4_Sample/Server.cpp
+++ RN4UE4_Sample/Source/RN4UE4_Sample/Server.cpp
@@ -82,198 +82,261 @@ void AServer::Tick( float DeltaTime )
 
 	if (bCanRecieve) 
 	{
-		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
-		{
-			switch (packet->data[0])
-			{
-				case ID_TEST_MESSAGE:
-				{
-					RakNet::RakString rs = RakNet::RakString();
-					RakNet::BitStream bsIn(packet->data, packet->length, false);
-					bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-					bsIn.Read(rs);
-					GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("HI!"));
-					break;
-				}
-				case ID_NEW_INCOMING_CONNECTION:
-				{
-					GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("INCOMNNING"));
-					RakNet::BitStream bsOut;
-
-					numOfPlayers++;
-
-
-					bsOut.Write((RakNet::MessageID)ID_GIVE_PLAYER_NUMBER);
-					bsOut.Write(numOfPlayers);
-
-					//yo simon this part dont even run
-					for (int i = 0; i < clients.Num(); i++)
-					{
-						RakNet::BitStream bsOutTwo;
-						bsOutTwo.Write((RakNet::MessageID)ID_ADD_PLAYER);
-						bsOutTwo.Write(numOfPlayers);
-						peer->Send(&bsOutTwo, HIGH_PRIORITY, RELIABLE_ORDERED, 1, clients[i], false);
-					}
-
-					TArray<AActor*> out;
-					UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
-
-					//yo simon this part dont even run
-					for (int i = 0; i < out.Num(); i++)
-					{
-						AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
-						RakNet::BitStream bsOutThree;
-						bsOutThree.Write((RakNet::MessageID)ID_GIVE_NEW_PLAYER_INFO);
-						bsOutThree.Write(out[i]->GetActorLocation());
-						bsOutThree.Write(actor->playerNum);
-						peer->Send(&bsOutThree, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
-					}
-
-					clients.Add(packet->systemAddress);
-					peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
-					break;
-				}
-				case ID_GET_NUMBER_PLAYERS:
-				{
-
-					//RakNet::BitStream bsOut;
-
-					//bsOut.Write((RakNet::MessageID)ID_GET_NUMBER_PLAYERS);
-					//bsOut.Write(numOfPlayers);
-					//bsOut.Write(MAX_CLIENTS);
-					//peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
-					break;
-				}
-				case ID_INPUT_MOVE_FORWARD_MESSAGE:
-				{
-					int num = 0;
-
-					FVector location(0.0f, 0.0f, 0.0f);
-					float input;
-
-					TArray<AActor*> out;
-					UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
-
-					RakNet::RakString rs = RakNet::RakString();
-					RakNet::BitStream bsIn(packet->data, packet->length, false);
-					bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-					bsIn.Read(input);
-					bsIn.Read(num);
-
-					for (int i = 0; i < out.Num(); i++)
-					{
-						AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
-						if (actor->playerNum == num)
-						{
-							actor->SetActorLocation(actor->GetActorLocation() + (input * FVector(1, 0, 0) * cubeSpeed * GetWorld()->GetDeltaSeconds()));
-							location = actor->GetActorLocation();
-						}
-					}
-					break;
-				}
-				case ID_INPUT_MOVE_RIGHT_MESSAGE:
-				{
-					int num = 0;
-
-					FVector location(0.0f, 0.0f, 0.0f);
-					float input;
-
-					TArray<AActor*> out;
-					UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
-
-					RakNet::RakString rs = RakNet::RakString();
-					RakNet::BitStream bsIn(packet->data, packet->length, false);
-					bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-					bsIn.Read(input);
-					bsIn.Read(num);
-
-					for (int i = 0; i < out.Num(); i++)
-					{
-						AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
-						if (actor->playerNum == num)
-						{
-							actor->SetActorLocation(actor->GetActorLocation() + (input * FVector(0, 1, 0) * cubeSpeed * GetWorld()->GetDeltaSeconds()));
-							location = actor->GetActorLocation();
-						}
-					}
-					break;
-				}
-				case ID_JUMP_INPUT:
-				{
-					int num = 0;
-
-					TArray<AActor*> out;
-					UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
-
-					RakNet::RakString rs = RakNet::RakString();
-					RakNet::BitStream bsIn(packet->data, packet->length, false);
-					bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-					bsIn.Read(num);
-
-					for (int i = 0; i < out.Num(); i++)
-					{
-						AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
-						if (actor->playerNum == num)
-						{
-							actor->bShouldJump = true;
-						}
-					}
-					break;
-				}
-			}
-		}
+		GoThroughRecievedPackets();
 	}
 
 }
 
-// This function starts up the server
+
+// Purpose : Called to start the server
+// In : None
+// Out : None
 void AServer::StartupServer() 
 {
+	// Create peer and reset packet
 	peer = RakNet::RakPeerInterface::GetInstance();
 	packet = NULL;
 	
+	// Startup the actual server
 	RakNet::SocketDescriptor sd(SERVER_PORT, 0);
 	peer->Startup(MAX_CLIENTS, &sd, 1);
+
+	// Set max clients
 	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	
+	// Print for proof
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Starting server!"));
 
+	// Set server to recieve
 	bCanRecieve = true;
 }
 
 
-// This function shuts down the server
+// Purpose : Called to shutdown the actual server
+// In : None
+// Out : None
 void AServer::ShutdownServer() 
 {
+	// Print for proof
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Shutting down server!"));
+	
+	// Make sure interface is valid
 	if (peer != nullptr)
 	{
+		// Shutdown and destroy interface
 		peer->Shutdown(300);
 		RakNet::RakPeerInterface::DestroyInstance(peer);
 	}
 }
 
-// This function shuts down the server
+
+// Purpose : Called to start the actual game
+// In : None
+// Out : None
 void AServer::StartGame()
 {
+	// Startup peer and set packet to null
 	peer = RakNet::RakPeerInterface::GetInstance();
 	packet = NULL;
 	char str[512] = "";
 
+	//Create a bitstream for packet data
 	RakNet::BitStream bsOut;
+
+	// Write message
 	bsOut.Write((RakNet::MessageID)ID_START_GAME);
 
+	// Write the string with message
 	bsOut.Write(str);
+
+	// Send to recieving client
 	peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
 }
 
+
+// Purpose : Called if a player has crossed the finish line to declare a winner
+// In : playerNum(The player number who won)
+// Out : None
 void AServer::DeclareWinner(int playerNum) 
 {
+	// Cycle thru all clients
 	for (int i = 0; i < clients.Num(); i++) 
 	{
+		// Get current packet
 		packet = peer->Receive();
+
+		// Create bitstream for packet dta
 		RakNet::BitStream bsOut;
+
+		// Write message identifier
 		bsOut.Write((RakNet::MessageID)ID_DECLARE_WINNER);
+		
+		// write a boolean to give the client the chance to see if the winner is them
 		bsOut.Write(i + 1 == playerNum);
+
+		// Send packet to given client
 		peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, clients[i], false);
+	}
+}
+
+
+// Purpose : To cycle through all of the custom messages to get the correct message
+// In : None
+// Out : None
+void AServer::GoThroughRecievedPackets()
+{
+	// Cycle thru recieved packets
+	for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+	{
+		// switch on packet data
+		switch (packet->data[0])
+		{
+
+		// Run for test message
+		case ID_TEST_MESSAGE:
+		{
+			RakNet::RakString rs = RakNet::RakString();
+			RakNet::BitStream bsIn(packet->data, packet->length, false);
+			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+			bsIn.Read(rs);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("HI!"));
+			break;
+		}
+
+		// Run for incoming connection
+		case ID_NEW_INCOMING_CONNECTION:
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("INCOMNNING"));
+			RakNet::BitStream bsOut;
+
+			numOfPlayers++;
+
+
+			bsOut.Write((RakNet::MessageID)ID_GIVE_PLAYER_NUMBER);
+			bsOut.Write(numOfPlayers);
+
+			//yo simon this part dont even run
+			for (int i = 0; i < clients.Num(); i++)
+			{
+				RakNet::BitStream bsOutTwo;
+				bsOutTwo.Write((RakNet::MessageID)ID_ADD_PLAYER);
+				bsOutTwo.Write(numOfPlayers);
+				peer->Send(&bsOutTwo, HIGH_PRIORITY, RELIABLE_ORDERED, 1, clients[i], false);
+			}
+
+			TArray<AActor*> out;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
+
+			//yo simon this part dont even run
+			for (int i = 0; i < out.Num(); i++)
+			{
+				AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
+				RakNet::BitStream bsOutThree;
+				bsOutThree.Write((RakNet::MessageID)ID_GIVE_NEW_PLAYER_INFO);
+				bsOutThree.Write(out[i]->GetActorLocation());
+				bsOutThree.Write(actor->playerNum);
+				peer->Send(&bsOutThree, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
+			}
+
+			clients.Add(packet->systemAddress);
+			peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
+			break;
+		}
+
+		// Run for getting player numbers
+		case ID_GET_NUMBER_PLAYERS:
+		{
+
+			//RakNet::BitStream bsOut;
+
+			//bsOut.Write((RakNet::MessageID)ID_GET_NUMBER_PLAYERS);
+			//bsOut.Write(numOfPlayers);
+			//bsOut.Write(MAX_CLIENTS);
+			//peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 1, packet->systemAddress, false);
+			break;
+		}
+
+		// Run for moving forward
+		case ID_INPUT_MOVE_FORWARD_MESSAGE:
+		{
+			int num = 0;
+
+			FVector location(0.0f, 0.0f, 0.0f);
+			float input;
+
+			TArray<AActor*> out;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
+
+			RakNet::RakString rs = RakNet::RakString();
+			RakNet::BitStream bsIn(packet->data, packet->length, false);
+			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+			bsIn.Read(input);
+			bsIn.Read(num);
+
+			for (int i = 0; i < out.Num(); i++)
+			{
+				AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
+				if (actor->playerNum == num)
+				{
+					actor->SetActorLocation(actor->GetActorLocation() + (input * FVector(1, 0, 0) * cubeSpeed * GetWorld()->GetDeltaSeconds()));
+					location = actor->GetActorLocation();
+				}
+			}
+			break;
+		}
+
+		// Run for moving right
+		case ID_INPUT_MOVE_RIGHT_MESSAGE:
+		{
+			int num = 0;
+
+			FVector location(0.0f, 0.0f, 0.0f);
+			float input;
+
+			TArray<AActor*> out;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
+
+			RakNet::RakString rs = RakNet::RakString();
+			RakNet::BitStream bsIn(packet->data, packet->length, false);
+			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+			bsIn.Read(input);
+			bsIn.Read(num);
+
+			for (int i = 0; i < out.Num(); i++)
+			{
+				AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
+				if (actor->playerNum == num)
+				{
+					actor->SetActorLocation(actor->GetActorLocation() + (input * FVector(0, 1, 0) * cubeSpeed * GetWorld()->GetDeltaSeconds()));
+					location = actor->GetActorLocation();
+				}
+			}
+			break;
+		}
+
+		// Run for jumping
+		case ID_JUMP_INPUT:
+		{
+			int num = 0;
+
+			TArray<AActor*> out;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), AReplicationActor::StaticClass(), out);
+
+			RakNet::RakString rs = RakNet::RakString();
+			RakNet::BitStream bsIn(packet->data, packet->length, false);
+			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+			bsIn.Read(num);
+
+			for (int i = 0; i < out.Num(); i++)
+			{
+				AReplicationActor * actor = Cast<AReplicationActor>(out[i]);
+				if (actor->playerNum == num)
+				{
+					actor->bShouldJump = true;
+				}
+			}
+			break;
+		}
+		}
 	}
 }
--- RN4UE4_Sample/Source/RN4UE4_Sample/Server.h
+++ RN4UE4_Sample/Source/RN4UE4_Sample/Server.h
@@ -22,49 +22,55 @@ class RN4UE4_SAMPLE_API AServer : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
+public:
 	// Sets default values for this actor's properties
 	AServer();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	virtual void Tick(float DeltaSeconds) override;
 
-	// Called to start server
-	UFUNCTION(BlueprintCallable, Category = "RakNet") 
-	void StartupServer();
+public:	
 
-	// Called to shut down server
-	UFUNCTION(BlueprintCallable, Category = "RakNet")
-	void ShutdownServer();
+	// Purpose : Called to start the server
+	// In : None
+	// Out : None
+	UFUNCTION(BlueprintCallable, Category = "RakNet") void StartupServer();
 
-	UFUNCTION(BlueprintCallable, Category = "Raknet")
-	void DeclareWinner(int playerNum);
+	// Purpose : Called to shutdown the actual server
+	// In : None
+	// Out : None
+	UFUNCTION(BlueprintCallable, Category = "RakNet") void ShutdownServer();
 
-	// Called to shut down server
-	UFUNCTION(BlueprintCallable, Category = "RakNet")
-	void StartGame();
+	// Purpose : Called if a player has crossed the finish line to declare a winner
+	// In : playerNum(The player number who won)
+	// Out : None
+	UFUNCTION(BlueprintCallable, Category = "Raknet") void DeclareWinner(int playerNum);
 
-	UPROPERTY(EditAnywhere)
-	TSubclassOf<AReplicationActor> replication;
+	// Purpose : Called to start the actual game
+	// In : None
+	// Out : None
+	UFUNCTION(BlueprintCallable, Category = "RakNet") void StartGame();
 
-	UPROPERTY(EditAnywhere)
-	float cubeSpeed;
+	// Purpose : To cycle through all of the custom messages to get the correct message
+	// In : None
+	// Out : None
+	UFUNCTION(BlueprintCallable, Category = "RakNet") void GoThroughRecievedPackets();
+
+	/* Public Vars */
+	UPROPERTY(EditAnywhere) float cubeSpeed; // Represents the speed the cube can actually go
 
 private:
-	RakNet::RakPeerInterface* peer;
-	RakNet::Packet* packet;
 
-	TArray<RakNet::SystemAddress> clients;
+	/* Raknet Vals */
+	RakNet::RakPeerInterface* peer; // Represents the interface with raknet
+	RakNet::Packet* packet; // Represents the current packet being sent or recieved
+	TArray<RakNet::SystemAddress> clients; // Represents all current clients on the server
 
-	UPROPERTY()
-	int numOfPlayers = 0;
-
-	UPROPERTY()
-	bool bCanRecieve = false;
-
-	UPROPERTY()
-	FVector repLocation = FVector(0.0f, 0.0f, 0.0f);
+	/* Private Vars */
+	UPROPERTY() int numOfPlayers = 0; // Represents the number of players currently on the server
+	UPROPERTY() bool bCanRecieve = false; // Represents if the server can currently recieve packets
+	UPROPERTY() FVector repLocation = FVector(0.0f, 0.0f, 0.0f); // Represents the replicated actor's location
 };
