89b76edce096c189772ea6ef93f57fd0c42ddb43
Simon Steele <simon.steele@mymail.champlain.edu>
2021-05-02 21:18:51 -0400

    See previous commit

--- RN4UE4_Sample/Source/RN4UE4_Sample/Client.cpp
+++ RN4UE4_Sample/Source/RN4UE4_Sample/Client.cpp
@@ -158,7 +158,7 @@ void AClient::Tick( float DeltaTime )
 
 					AReplicationActor * newActor = GetWorld()->SpawnActor<AReplicationActor>(replication, Location, Rotation, SpawnInfo);
 					newActor->playerNum = num;
-					newActor->bIsOwner = num == 1;
+					newActor->bIsOwner = playerNumber == 1;
 					break;
 				}
 				case ID_UPDATE_TRANSFORM:
